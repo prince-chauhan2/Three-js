29e086b5a870c6cb80ae547384217837eb346b2b
prince-chauhan2 <theprincec17@gmail.com>
2026-04-01 09:27:14 +0530

    Change the file structure

--- Learning phase/Simple_rotating_cube/p1try.js
+++ Learning phase/Simple_rotating_cube/p1try.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 // 2. THE CAMERA: How we view the world
 // PerspectiveCamera(Field of View, Aspect Ratio, Near Clipping, Far Clipping)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5; // Move the camera back so we can see the center
+camera.position.z = 20; // Move the camera back so we can see the center
 
 // 3. THE RENDERER: The engine that draws the pixels
 const renderer = new THREE.WebGLRenderer();
@@ -17,12 +17,12 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement); // Add the "canvas" to your HTML
 
 // 4. THE OBJECT: Let's make a simple red cube
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(4, 5, 6);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-// 5. THE ANIMATION LOOP: This runs 60 times per second
+// 5. THE ANIMATION LOOP: This runs 60 tim                  es per second
 function animate() {
     requestAnimationFrame(animate);
 
